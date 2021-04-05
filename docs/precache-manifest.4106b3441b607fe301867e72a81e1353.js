7c72e94d892dc4d53d86ffbba8b176494cee7443
Aline Ovando <celes.rossette@gmail.com>
2021-04-04 19:09:40 -0500

    agregamos docs

---
+++ docs/precache-manifest.4106b3441b607fe301867e72a81e1353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468c8a8b6aea8aa7fe2d7914718473ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5acd0988140f4b3e7ddb",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5acd0988140f4b3e7ddb",
+    "url": "/static/js/main.36bc356e.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
